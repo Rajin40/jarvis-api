e34498259b06192c18c85043b47e658c36736b13
Rajin Shale <119264132+Rajin40@users.noreply.github.com>
2025-06-04 03:49:44 +0600

    Update function_intregation.py

--- Chrome_Intregretion/function_intregation.py
+++ Chrome_Intregretion/function_intregation.py
@@ -2,8 +2,7 @@ import sys
 sys.path.append("D:/python/jervis")
 from backend.Temperature import temp
 from Chrome_Intregretion.CLOCK import what_is_the_time
-from Chrome_Intregretion.find_my_ip import *
-import pyautogui as gui
+from Chrome_Intregretion.find_my_ip import find_ip
 import webbrowser
 from Fiching_email.Google_map_data_scap import scrape_google_maps
 from Fiching_email.email_extractor import extract_emails_from_websites
@@ -13,8 +12,6 @@ from Linkedin_file.Linkedin_Article_store import analyze_and_store_for_linkedin
 from Website_automation.blog_store import analyze_and_store_content_for_website
 from Website_automation.post_blog_in_website import post_category_blog
 from Generate_code.knowledge_updater import code_gen_mode,list_skills,view_error_log,auto_fix_plugins
-import time
-import json
 
 def split_compound_commands(text):
     """Split compound commands into individual commands"""
@@ -243,109 +240,6 @@ def Function_cmd(cmd):
         webbrowser.open(f"https://www.{cmd.split()[-1]}.com")
         print(f"Visiting {cmd.split()[-1]}")
         return True
-        
-    elif any(trigger in cmd for trigger in ['play', 'pause', 'stop']):
-        gui.hotkey("space")
-        print("Media control executed")
-        return True
-        
-    elif any(trigger in cmd for trigger in ['maximize window', 'maximize this']):
-        gui.hotkey("win", "up")
-        print("Maximizing window")
-        return True
-        
-    elif any(trigger in cmd for trigger in ['minimise window', 'minimise this', 'minimize it']):
-        gui.hotkey("win", "up")
-        print("Maximizing window")
-        return True
-        
-    elif 'restore window' in cmd:
-        gui.hotkey("win", "shift", "up")
-        print("Restoring window")
-        return True
-        
-    elif any(trigger in cmd for trigger in ['switch window', 'next window']):
-        gui.hotkey("alt", "tab")
-        print("Switching to next window")
-        return True
-        
-    elif any(trigger in cmd for trigger in ['previous window', 'back window']):
-        gui.hotkey("alt", "shift", "tab")
-        print("Switching to previous window")
-        return True
-        
-    elif any(trigger in cmd for trigger in ['open incognito', 'private window']):
-        gui.hotkey("ctrl", "shift", "n")
-        print("Opening incognito window")
-        return True
-        
-    elif any(trigger in cmd for trigger in ['bookmark page', 'save page']):
-        gui.hotkey("ctrl", "d")
-        print("Bookmarking page")
-        return True
-        
-    elif any(trigger in cmd for trigger in ['shutdown', 'turn off computer']):
-        gui.hotkey("win","d")
-        time.sleep(0.4)
-        gui.hotkey("alt", "f4")
-        time.sleep(0.4)
-        gui.press("enter")
-        print("Shutting down")
-        return True
-
-    # Text Editing
-    elif any(trigger in cmd for trigger in ['write', 'type']):
-        print("writing boss")
-        gui.write(cmd.replace("write", "").replace("type", "").strip())
-        print("Text written")
-        return True
-        
-    elif any(trigger in cmd for trigger in ['enter', 'press enter']):
-        gui.press("enter")
-        print("Enter pressed")
-        return True
-        
-    elif any(trigger in cmd for trigger in ['select all', 'select all this']):
-        print("done boss")
-        gui.hotkey("ctrl", "a")
-        print("All content selected")
-        return True
-        
-    elif any(trigger in cmd for trigger in ['copy', 'copy this']):
-        gui.hotkey("ctrl", "c")
-        print("Content copied")
-        return True
-        
-    elif any(trigger in cmd for trigger in ['paste', 'paste here']):
-        gui.hotkey("ctrl", "v")
-        print("Content pasted")
-        return True
-        
-    elif any(trigger in cmd for trigger in ['undo', 'undo karo', 'back', 'back karo']):
-        gui.hotkey("ctrl", "z")
-        print("Action undone")
-        return True
-        
-    elif any(trigger in cmd for trigger in ['cut', 'cut this']):
-        gui.hotkey("ctrl", "x")
-        print("Content cut")
-        return True
-        
-    elif any(trigger in cmd for trigger in ['redo', 'redo karo', 'forward', 'forward karo']):
-        gui.hotkey("ctrl", "y")
-        print("Action redone")
-        return True
-        
-    elif any(trigger in cmd for trigger in ['delete', 'delete this']):
-        gui.press("delete")
-        print("Content deleted")
-        return True
-        
-    elif any(trigger in cmd for trigger in ['backspace', 'remove']):
-        gui.press("backspace")
-        print("Character removed")
-        return True
-
     # Time
     elif "what is the time" in cmd or "time" in cmd or "what time is" in cmd:
         what_is_the_time()
@@ -354,6 +248,10 @@ def Function_cmd(cmd):
     # IP Address
     elif "find my ip" in cmd or "ip address" in cmd:
         print("your ip is " + find_ip())
+        return True
+    
+    elif "what is the temperature" in cmd or "temperature" in cmd or "what is temp" in cmd:
+        temp()
         return True
 
     # If no command matched
